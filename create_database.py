ea4b99a0ffd893b53b79b2b4974df288e1b8717b
naimdeb <naimdebard02@gmail.com>
2025-03-20 16:50:42 +0100

    fin journée

--- create_database.py
+++ create_database.py
@@ -5,13 +5,29 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain.document_loaders import DirectoryLoader
+import os
+import shutil
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 DATA_PATH = "data/books"
+CHROMA_PATH = "chroma"
+
+
+def main():
+    generate_data()
+
+
 
 
 def generate_data():
-    documents =
+    documents = load_documents()
+    chunks = split_text(documents)
+    # save_to_chroma
+
+
 
 
 # fonction load documents qui transforme tt le dossier en un seul document avec content et metadata
@@ -21,16 +37,35 @@ def load_documents():
     return documents
 
 # On appelle text splitter pour découper notre document en morceaux
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=500,
-    length_function=len,
-    add_start_index=True,
-)
+def split_text(documents):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=500,
+        length_function=len,
+        add_start_index=True,
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f'Découpé {len(DATA_PATH)} documents en {len(chunks)} morceaux')
 
-def split_text(documents: list[Document])
+    # document = chunks[10]
+    # print(document.page_content)
+    # print(document.metadata)
+
+    return chunks
 
 
-# On coupe tout
-chunks = text_splitter.split_documents(load_documents())
-print(f'Découpé {len(DATA_PATH)} documents en {len(chunks)} morceaux')
+def save_to_chroma(chunks):
+    #Clear the database
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+    # Create a new DB from the documents.
+    db = Chroma.from_documents(
+        chunks,  ,persist_directory=CHROMA_PATH
+    )
+    db.persist()
+    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
+
+
+if __name__ == "__main__":
+    main()
