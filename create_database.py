6e2502962b93e74e78c16fa8e7185e7ea3827015
naimdeb <naimdebard02@gmail.com>
2025-03-21 09:55:03 +0100

    done create database && added requirements && updated gitignore

--- create_database.py
+++ create_database.py
@@ -1,10 +1,11 @@
-from langchain_mistralai.chat_models import ChatMistralAI
+# Creates the Chroma vector database by cutting the documents into chunks, then the chunks into embeddings and storing it to the chroma database
+
+
 from langchain_mistralai.embeddings import MistralAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import create_retrieval_chain
 from langchain.document_loaders import DirectoryLoader
+from langchain_community.vectorstores import Chroma
+from langchain_mistralai.embeddings import MistralAIEmbeddings
 import os
 import shutil
 from dotenv import load_dotenv
@@ -25,7 +26,7 @@ def main():
 def generate_data():
     documents = load_documents()
     chunks = split_text(documents)
-    # save_to_chroma
+    save_to_chroma(chunks)
 
 
 
@@ -59,13 +60,15 @@ def save_to_chroma(chunks):
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
 
+    embeddings = MistralAIEmbeddings(model="mistral-embed")
+
     # Create a new DB from the documents.
     db = Chroma.from_documents(
-        chunks,  ,persist_directory=CHROMA_PATH
+        chunks, embeddings ,persist_directory=CHROMA_PATH
     )
-    db.persist()
     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
 
 
+# Lance la fonction main seulement quand on lance le script directement et pas en utilisant un module
 if __name__ == "__main__":
     main()
